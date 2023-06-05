2aeffd5853935685a13453005912e3743e9ba61f
maxrivett <maxnrivett@gmail.com>
2023-06-05 03:41:32 -0700

    Loading message to bypass slack timeout error

--- index.py
+++ index.py
@@ -3,10 +3,12 @@ from flask import Flask, request, jsonify
 import requests
 import json
 from slack_sdk import WebClient
+from slack_sdk.errors import SlackApiError
 import openai
 import random
 from dotenv import load_dotenv
 import os
+import threading
 
 # Initialize Flask app
 app = Flask(__name__)
@@ -26,8 +28,8 @@ IMGFLIP_USERNAME = os.getenv("IMGFLIP_USERNAME")
 IMGFLIP_PASSWORD = os.getenv("IMGFLIP_PASSWORD")
 
 # Prompts for GPT API
-PROMPT = "You are tasked with crafting a funny meme, using the %s meme format. Note that the meme should not be about the subject matter of the format. This meme will feature two text segments – one at the top and the other at the bottom. These two segments should be created by you and separated using the '|' symbol. The theme of the meme is '%s'. Maintain a light-hearted and humorous tone throughout. Please note, avoid using explicit labels like 'top text' or 'bottom text', and refrain from using quotation marks."
-PROMPT_NO_INPUT = "You are tasked with crafting a funny meme, using the %s meme format. Note that the meme should not be about the subject matter of the format. This meme will feature two text segments – one at the top and the other at the bottom. These two segments should be created by you and separated using the '|' symbol. Your task is to decide the theme and content of the meme. Maintain a light-hearted and humorous tone throughout. Please note, avoid using explicit labels like 'top text' or 'bottom text', and refrain from using quotation marks."
+PROMPT = "You are tasked with crafting a funny meme, using the %s meme format. Note that the meme should not be about the subject matter of the format, be sure to not include the meme format in any of the text. This meme will feature two text segments – one at the top and the other at the bottom. These two segments should be created by you and separated using the '|' symbol. The theme of the meme is '%s'. Maintain a light-hearted and humorous tone throughout. Please note, avoid using explicit labels like 'top text' or 'bottom text', and refrain from using quotation marks!"
+PROMPT_NO_INPUT = "You are tasked with crafting a funny meme, using the %s meme format. Note that the meme should not be about the subject matter of the format, be sure to not include the meme format in any of the text. This meme will feature two text segments – one at the top and the other at the bottom. These two segments should be created by you and separated using the '|' symbol. Your task is to decide the theme and content of the meme. Maintain a light-hearted and humorous tone throughout. Please note, avoid using explicit labels like 'top text' or 'bottom text', and refrain from using quotation marks!"
 
 # Function to get a random meme template ID from Imgflip
 def get_random_template_id():
@@ -50,14 +52,8 @@ def get_random_template_id():
 
     return random_meme['id'], random_meme['name']
 
-# Define a route for the command endpoint 
-@app.route('/meme-endpoint', methods=['POST'])
-def create_meme():
-    # Parse the request payload
-    payload = request.form
-
-    # Extract the user input from the payload and get a random meme template ID
-    user_input = payload['text']
+# Function to post the meme to chat (separated so that there is a loading message in the meantime)
+def generate_and_post_meme(user_input, username, channel_id, ts):
     template_id, description = get_random_template_id()
     if template_id == None :
         raise Exception("No template_id generated")
@@ -69,8 +65,8 @@ def create_meme():
     response = openai.Completion.create(
         engine="text-davinci-003",
         prompt=prompt,
-        temperature=0.5,
-        max_tokens=60
+        temperature=0.8,
+        max_tokens=150
     )
 
     # Choose "best" meme option
@@ -92,14 +88,52 @@ def create_meme():
     res = requests.post(url, params)
     meme_url = json.loads(res.text)['data']['url']
 
-    # Post meme to Slack channel
-    client.chat_postMessage(
-        channel=payload['channel_id'],
-        text="Here's your meme:" if user_input == "" else "Here's your meme about " + user_input + ":",
-        attachments=[{"image_url": meme_url, "text": meme_phrase}]
+    # Delete the "Creating your meme now..." message
+    client.chat_delete(
+        channel=channel_id,
+        ts=ts
     )
 
-    return jsonify(response_type='in_channel')
+    # Post meme to Slack channel
+    client.chat_postMessage(
+        channel=channel_id,
+        text="Here's your meme, " + username + ":" if user_input == "" else "Here's your meme about " + user_input + "," + username + ":",
+        attachments=[{"image_url": meme_url, "author_name": "_", "author_link": "https://www.maxrivett.com",}]
+    )
+
+        
+
+# Define a route for the command endpoint 
+@app.route('/meme-endpoint', methods=['POST'])
+#
+def create_meme():
+    # Parse the request payload
+    payload = request.form
+
+    # Extract the user input from the payload and get a random meme template ID
+    user_input = payload['text']
+    channel_id = payload['channel_id']
+
+    # Send the "Creating your meme now..." message
+    response = client.chat_postMessage(
+        channel=channel_id,
+        text='Creating your meme now...'
+    )
+
+    username = ''
+    # Get the user's username using the Slack API
+    try:
+        user_info = client.users_info(user=payload['user_id'])
+        username = user_info['user']['name']
+    except SlackApiError as e:
+        username = 'Unknown User'
+    
+    # Get the 'ts' of the message we just sent
+    ts = response['ts']
+
+    threading.Thread(target=generate_and_post_meme, args=(user_input, username, channel_id, ts)).start()
+
+    return '', 200  # Empty response, status code 200
 
 # Start the flask app
 if __name__ == "__main__":
